2daa190762ab516505036e9958789b836b8b85cb
ajnaduvil <ajnaduvil@gmail.com>
2020-09-13 13:47:48 +0530

    random note tests

---
+++ Assets/App/ShaderGraph/Learn/PostProcessing/ColorEffect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/ColorEffect")]
+public sealed class ColorEffect : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
+    public TextureParameter texture = new TextureParameter();
+}
+
+public sealed class GrayscaleRenderer : PostProcessEffectRenderer<ColorEffect>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Shader Graphs/ColorEffect"));
+        sheet.properties.SetTexture("_Input", context.camera.activeTexture);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
